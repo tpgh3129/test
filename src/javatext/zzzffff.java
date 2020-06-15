7cfc81e062cea65932b14d129d76c76d5c011041
tpgh3129 <55993282+tpgh3129@users.noreply.github.com>
2020-06-15 11:28:26 +0900

    commit

---
+++ src/javatext/zzzffff.java
@@ -0,0 +1,16 @@
+package javatext;
+
+public class zzzffff {
+	public static void main(String[] args) {
+		
+		for(int i=1; i <= 9; i++) {
+			System.out.println(i + "´Ü");
+			for(int j=1; j<=9; j++) {
+				
+				System.out.println(i + "*" + j + "=" + i *j);
+			}
+		}
+	}
+}
+	
+	
